b8a3fa2b4a0c19e56cb190b6f353e2cee397709a
Nick Howell <nick@nickhowell.co.uk>
2016-02-19 12:55:12 +0000

    Create links to atom config

--- install.ps1
+++ install.ps1
@@ -1,6 +1,7 @@
 ﻿Write-Output "Installing Chocolatey"
 iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
 
+Write-Output "Install applications"
 
 choco install googlechrome -y
 choco install 7zip -y
@@ -15,6 +16,15 @@ choco install winscp -y
 choco install poshgit -y
 choco install slack -y
 
+
+Write-Output "Setup links to Atom config and packages directory"
+
+mklink /J ~/.atom/packages $PSScriptRoot/.atom/packages
+mklink /H ~/.atom/config.cson $PSScriptRoot/.atom/config.cson
+mklink /H ~/.atom/init.coffee $PSScriptRoot/.atom/init.coffee
+mklink /H ~/.atom/keymap.cson $PSScriptRoot/.atom/keymap.cson
+mklink /H ~/.atom/snippets.cson $PSScriptRoot/.atom/snippets.cson
+mklink /H ~/.atom/styles.less $PSScriptRoot/.atom/styles.less
 
 
 
