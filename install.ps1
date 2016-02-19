52dcbf9b33bdf324b1f45f71ffa591488342cd47
Nick Howell <nick@nickhowell.co.uk>
2016-02-19 11:11:43 +0000

    Install chocolatey and applications

---
+++ install.ps1
@@ -0,0 +1,22 @@
+﻿Write-Output "Setting execution policy"
+Set-ExecutionPolicy RemoteSigned
+
+Write-Output "Installing Chocolatey"
+iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
+
+
+choco install googlechrome
+choco install 7zip
+choco install git -params '"/noAutoCrlf"'
+choco install gitextensions
+choco install vlc
+choco install atom
+choco install keepass
+choco install putty
+choco install dropbox
+choco install winscp
+choco install poshgit
+
+
+
+
