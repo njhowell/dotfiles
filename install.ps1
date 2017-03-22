4cdaf5d8b2c33e8342f92fcbb0c8078cf31ba0f4
Nick Howell <nick@nickhowell.co.uk>
2017-03-22 11:39:55 +0000

    Add a couple of new tools to the list

--- install.ps1
+++ install.ps1
@@ -5,12 +5,13 @@ Write-Output "Install applications"
 
 choco install googlechrome -y
 choco install 7zip -y
-choco install gitextensions -y
 choco install vlc -y
 choco install atom -y
 choco install winscp -y
 choco install poshgit -y
 choco install slack -y
+choco install conemu -y
+choco install gitkraken -y
 
 
 If(Test-Path $home\.atom)
