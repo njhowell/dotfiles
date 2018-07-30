19b5784f026277dcd8b676771d0645884a41c91e
Nick Howell <Nick.Howell@red-gate.com>
2018-07-30 16:10:25 +0100

    Add pagaent shortcut to auto load ssh keys on startup

--- puppet/modules/common/manifests/init.pp
+++ puppet/modules/common/manifests/init.pp
@@ -1,14 +1,19 @@
 class common {
     include chocolatey
-    
-    
+
+
     file {'c:/users/nick.howell/Documents/WindowsPowerShell':
         ensure => directory,
     }
-    
+
     file {'c:/users/nick.howell/Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1':
         ensure => present,
         source => 'puppet:///modules/common/Microsoft.PowerShell_profile.ps1'
     }
-    
+
+    file {'C:/Users/nick.howell/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup/pageant.lnk':
+      ensure => present,
+      source => 'puppet:///modules/common/pageant.lnk'
+    }
+
 }
