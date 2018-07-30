31cd51b985a18f02cfaedad989711d79d395186a
Nick Howell <Nick.Howell@red-gate.com>
2018-07-30 11:48:02 +0100

    Setup Powershell profile

---
+++ puppet/modules/common/files/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,16 @@
+﻿
+Import-Module 'C:\tools\poshgit\dahlbyk-posh-git-9bda399\src\posh-git.psd1'
+
+# Load Office 365 helper
+. 'C:\Users\nick.howell\Documents\git\IS-Scripts\PowerShell Profile Helpers\Connect-Office365.ps1'
+
+# Load Exchange Hybrid Helper
+. 'C:\Users\nick.howell\Documents\git\IS-Scripts\PowerShell Profile Helpers\Connect-ExchangeHybrid.ps1'
+
+# Load AD Sync Helper
+. 'C:\Users\nick.howell\Documents\git\IS-Scripts\PowerShell Profile Helpers\Sync-Office365.ps1'
+# Chocolatey profile
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
--- puppet/modules/common/manifests/init.pp
+++ puppet/modules/common/manifests/init.pp
@@ -1,3 +1,14 @@
 class common {
     include chocolatey
+    
+    
+    file {'c:/users/nick.howell/Documents/WindowsPowerShell':
+        ensure => directory,
+    }
+    
+    file {'c:/users/nick.howell/Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1':
+        ensure => present,
+        source => 'puppet:///modules/common/Microsoft.PowerShell_profile.ps1'
+    }
+    
 }
