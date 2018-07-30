53f9ae102d059817b145e25dee7b7ea89acf8a27
Nick Howell <Nick.Howell@red-gate.com>
2018-07-30 16:09:48 +0100

    install dropbox, fix git install

--- puppet/modules/software/manifests/init.pp
+++ puppet/modules/software/manifests/init.pp
@@ -5,38 +5,37 @@ class software {
         install_options => ['--ignore-checksums'],
         require => Class['chocolatey']
     }
-    
+
     package { 'git':
         ensure          => 'latest',
         provider        => 'chocolatey',
-        install_options => ['-params "/noAutoCrlf"'],
         require => Class['chocolatey']
     }
-    
+
     package { 'putty.install':
         ensure          => 'latest',
         provider        => 'chocolatey',
         require => Class['chocolatey']
     }
-    
+
     package { '7zip':
         ensure          => 'latest',
         provider        => 'chocolatey',
         require => Class['chocolatey']
     }
-    
+
     package { 'vlc':
         ensure          => 'latest',
         provider        => 'chocolatey',
         require => Class['chocolatey']
     }
-    
+
     package { 'atom':
         ensure          => 'latest',
         provider        => 'chocolatey',
         require => Class['chocolatey']
     }
-    
+
     package { 'winscp':
         ensure          => 'latest',
         provider        => 'chocolatey',
@@ -52,47 +51,52 @@ class software {
         provider        => 'chocolatey',
         require => Class['chocolatey']
     }
-    
+
     package { 'conemu':
         ensure          => 'latest',
         provider        => 'chocolatey',
         require => Class['chocolatey']
     }
-    
+
     package { 'gitkraken':
         ensure          => 'latest',
         provider        => 'chocolatey',
         require => Class['chocolatey']
     }
-    
+
     package { 'launchy':
         ensure          => 'latest',
         provider        => 'chocolatey',
         require => Class['chocolatey']
     }
-    
+
     package { 'terminals':
         ensure          => 'latest',
         provider        => 'chocolatey',
         require => Class['chocolatey']
     }
-    
+
     package { 'vagrant':
         ensure          => 'latest',
         provider        => 'chocolatey',
         require => Class['chocolatey']
     }
-    
+
     package { 'virtualbox':
         ensure          => 'latest',
         provider        => 'chocolatey',
         require => Class['chocolatey']
     }
-    
+
     package { 'vmwareworkstation':
         ensure          => 'latest',
         provider        => 'chocolatey',
         require => Class['chocolatey']
     }
-    
+
+    package { 'dropbox':
+        ensure          => 'latest',
+        provider        => 'chocolatey',
+        require => Class['chocolatey']
+    }
 }
