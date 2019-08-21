44e1876d3003948b62c271a1e17d41d3dd16e656
Nick Howell <nick@nickhowell.co.uk>
2019-08-21 16:10:21 +0100

    load openstack helper

--- puppet/modules/common/files/Microsoft.PowerShell_profile.ps1
+++ puppet/modules/common/files/Microsoft.PowerShell_profile.ps1
@@ -9,6 +9,8 @@ Import-Module 'C:\tools\poshgit\dahlbyk-posh-git-9bda399\src\posh-git.psd1'
 
 # Load AD Sync Helper
 . 'C:\Users\nick.howell\Documents\git\IS-Scripts\PowerShell Profile Helpers\Sync-Office365.ps1'
+# Load Openstack Helper
+. 'C:\Users\nick.howell\Documents\git\IS-Scripts\PowerShell Profile Helpers\Connect-Openstack.ps1'
 # Chocolatey profile
 $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
 if (Test-Path($ChocolateyProfile)) {
